aec04f6ca4a0765aa75d78328cc4c12aa7e33e65
LainusSG <191011022@acatlan.tecnm.mx>
2023-12-13 17:59:58 -0600

    Tracy

--- core/core/urls.py
+++ core/core/urls.py
@@ -24,9 +24,21 @@ from .views import (
     home_view,
     find_user_view,
     logint_view,
+    
+    #-------------------modulo 1--------------------
     programacionC_view,
     datosDC,
     creacionP,
+    
+    #-------------------recepción--------------------
+    incidencias,
+    materialdequirofano,
+    otrasrecepciones,
+    otrasrecepciones2,
+    otrasunidadeshospitalarias,
+    proveedorexterno,
+    recepciondeprovedorexterno,
+    recepcion,
 
 
 )
@@ -38,9 +50,21 @@ urlpatterns = [
     path('reconocimiento-facial/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('classify/', find_user_view, name='classify'),
+
+    #-------------------modulo 1--------------------
     path ('programacion-de-cirugia/', programacionC_view, name='programacionC'),
     path ('creacion-de-paquetes/', creacionP, name='programacionC'),
     path ('datos-de-programacion/', datosDC, name='programacionC'),
+
+     #-------------------recepción--------------------
+     path ('incidencias/', incidencias, name='incidencias'),
+     path ('material-de-quirofano/', materialdequirofano, name='material-de-quirofano'),
+     path ('otras-recepciones/', otrasrecepciones, name='otras-recepciones'),
+     path ('otras-recepciones2/', otrasrecepciones2, name='otras-recepciones'),
+     path ('otras-unidades-hospitalarias/', otrasunidadeshospitalarias, name='otras-unidades-hospitalarias'),
+     path ('proveedor-externo/', proveedorexterno, name='proveedor-externo'),
+     path ('recepcion-de-provedor-externo/', recepciondeprovedorexterno, name='recepcion-de-provedor-externo'),
+     path ('recepcion/', recepcion, name='recepcion'),
   
 ]
 
